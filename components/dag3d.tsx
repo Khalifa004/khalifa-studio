100d8a1c6d5967fe35df0b3f25592e52f99a4e34
Khalifa <khalifa.seck@protonmail.com>
2025-08-19 17:41:20 -0400

    vg

--- components/dag3d.tsx
+++ components/dag3d.tsx
@@ -9,34 +9,32 @@ export function DraggableCardDemo() {
     {
       title: "Max Flyo",
       image: "/images/art-1.jpg",
-      className: "absolute top-10 left-[20%] rotate-[-5deg]",
+      className: "absolute top-20 left-[10%] rotate-[-5deg]",
     },
     {
       title: "Khaz Headphones",
       image: "/images/art-2.jpg",
-      className: "absolute top-40 left-[25%] rotate-[-7deg]",
+      className: "absolute top-32 right-[15%] rotate-[-7deg]",
     },
     {
       title: "Madrid",
       image: "/images/art-3.jpg",
-      className: "absolute top-5 left-[40%] rotate-[8deg]",
+      className: "absolute bottom-32 left-[12%] rotate-[8deg]",
     },
     {
       title: "Dystopian Station",
       image: "/images/art-4.jpg",
-      className: "absolute top-32 left-[55%] rotate-[10deg]",
+      className: "absolute bottom-20 right-[10%] rotate-[10deg]",
     },
-  
     {
       title: "Drone",
       image: "/images/art-6.jpg",
-      className: "absolute top-24 left-[45%] rotate-[-7deg]",
+      className: "absolute top-1/2 left-[8%] -translate-y-1/2 rotate-[-7deg]",
     },
-
   ];
   return (
     <DraggableCardContainer className="relative flex min-h-screen w-full items-center justify-center overflow-clip">
-      <p className="absolute top-1/2 mx-auto max-w-2xl -translate-y-3/4 text-center text-5xl md:text-7xl lg:text-8xl font-black leading-tight tracking-tight ">
+      <p className="absolute top-1/2 mx-auto max-w-2xl -translate-y-3/4 text-center text-5xl md:text-7xl lg:text-8xl font-black leading-tight tracking-tight z-20">
         <span className="bg-gradient-to-r from-purple-600 via-pink-600 to-blue-600 bg-clip-text text-transparent drop-shadow-2xl">
           Sometimes I do
         </span>
